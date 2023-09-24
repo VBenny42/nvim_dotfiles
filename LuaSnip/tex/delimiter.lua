6e4df6cd93517f4c60bd02e4b85998666691f0e1
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-09-23 23:05:47 -0400

    feat: fold text objects and fixed tex snippet

--- LuaSnip/tex/delimiter.lua
+++ LuaSnip/tex/delimiter.lua
@@ -6,7 +6,7 @@ local get_visual = function(args, parent)
   end
 end
 
--- Math context detection 
+-- Math context detection
 local tex = {}
 tex.in_mathzone = function() return vim.fn['vimtex#syntax#in_mathzone']() == 1 end
 tex.in_text = function() return not tex.in_mathzone() end
@@ -15,77 +15,77 @@ tex.in_text = function() return not tex.in_mathzone() end
 return
 {
   -- LEFT/RIGHT PARENTHESES
-  s({trig = "([^%a])l%(", regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  s({ trig = "([^%a])l%(", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmta(
-      "<>\\left(<>\\right)",
+      "<>\\left(<>\\right",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- LEFT/RIGHT SQUARE BRACES
-  s({trig = "([^%a])l%[", regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  s({ trig = "([^%a])l%[", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmta(
       "<>\\left[<>\\right]",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- LEFT/RIGHT CURLY BRACES
-  s({trig = "([^%a])l%{", regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  s({ trig = "([^%a])l%{", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmta(
       "<>\\left\\{<>\\right\\}",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- BIG PARENTHESES
-  s({trig = "([^%a])b%(", regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  s({ trig = "([^%a])b%(", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmta(
       "<>\\big(<>\\big)",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- BIG SQUARE BRACES
-  s({trig = "([^%a])b%[", regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  s({ trig = "([^%a])b%[", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmta(
       "<>\\big[<>\\big]",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- BIG CURLY BRACES
-  s({trig = "([^%a])b%{", regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  s({ trig = "([^%a])b%{", regTrig = true, wordTrig = false, snippetType = "autosnippet" },
     fmta(
       "<>\\big\\{<>\\big\\}",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- ESCAPED CURLY BRACES
-  s({trig = "([^%a])\\%{", regTrig = true, wordTrig = false, snippetType="autosnippet", priority=2000},
+  s({ trig = "([^%a])\\%{", regTrig = true, wordTrig = false, snippetType = "autosnippet", priority = 2000 },
     fmta(
       "<>\\{<>\\}",
       {
-        f( function(_, snip) return snip.captures[1] end ),
+        f(function(_, snip) return snip.captures[1] end),
         d(1, get_visual),
       }
     )
   ),
   -- LATEX QUOTATION MARK
-  s({trig = "``", snippetType="autosnippet"},
+  s({ trig = "``", snippetType = "autosnippet" },
     fmta(
       "``<>''",
       {
@@ -94,4 +94,3 @@ return
     )
   ),
 }
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,113 +8,114 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     'nvim-treesitter/playground',
   },
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = { "c", "lua", "javascript", "typescript", "vim", "vimdoc", "python" },
+  opts = {
+    ensure_installed = { "c", "lua", "javascript", "typescript", "vim", "vimdoc", "python" },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
+    highlight = {
+      -- `false` will disable the whole extension
+      enable = true,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-        disable = { "latex" },
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+      disable = { "latex" },
+    },
+    indent = { enable = true, disable = { 'python' } },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-space>',
+        node_incremental = '<c-space>',
+        scope_incremental = '<c-s>',
+        node_decremental = '<c-backspace>',
       },
-      indent = { enable = true, disable = { 'python' } },
-      incremental_selection = {
+    },
+    -- textsubjects = {
+    --   enable = true,
+    --   prev_selection = ',',
+    --   keymaps = {
+    --     ['.'] = 'textsubjects-smart',
+    --     [';'] = 'textsubjects-container-outer',
+    --     [';i'] = 'textsubjects-container-inner',
+    --   },
+    -- },
+    textobjects = {
+      select = {
         enable = true,
+        lookahead = true,
         keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<c-backspace>',
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
         },
       },
-      -- textsubjects = {
-      --   enable = true,
-      --   prev_selection = ',',
-      --   keymaps = {
-      --     ['.'] = 'textsubjects-smart',
-      --     [';'] = 'textsubjects-container-outer',
-      --     [';i'] = 'textsubjects-container-inner',
-      --   },
-      -- },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-            [']a'] = '@parameter.inner',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-            [']A'] = '@parameter.inner',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-            ['[a'] = '@parameter.inner',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-            ['[A'] = '@parameter.inner',
-          },
-        },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_previous = {
-            ['<leader>A'] = '@parameter.inner',
-          },
-        },
-      },
-      playground = {
+      move = {
         enable = true,
-        disable = {},
-        updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = 'o',
-          toggle_hl_groups = 'i',
-          toggle_injected_languages = 't',
-          toggle_anonymous_nodes = 'a',
-          toggle_language_display = 'I',
-          focus_language = 'f',
-          unfocus_language = 'F',
-          update = 'R',
-          goto_node = '<cr>',
-          show_help = '?',
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+          [']a'] = '@parameter.inner',
         },
-      }
-    })
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+          [']A'] = '@parameter.inner',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+          ['[a'] = '@parameter.inner',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+          ['[A'] = '@parameter.inner',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+    },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = 'o',
+        toggle_hl_groups = 'i',
+        toggle_injected_languages = 't',
+        toggle_anonymous_nodes = 'a',
+        toggle_language_display = 'I',
+        focus_language = 'f',
+        unfocus_language = 'F',
+        update = 'R',
+        goto_node = '<cr>',
+        show_help = '?',
+      },
+    }
+  },
+  config = function(_, opts)
+    require('nvim-treesitter.configs').setup(opts)
   end
 }
