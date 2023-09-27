5dfe9f2cf166bdc878a23560e18cca140c59384e
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-09-26 23:58:42 -0400

    feat: consolidate git plugins

--- lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'tpope/vim-fugitive',
-  keys = {
-    { '<leader>gs', vim.cmd.Git,                 desc = 'show [G]it' },
-    { '<leader>gb', '<cmd>silent GBrowse .<CR>', desc = '[G]it [B]rowse ' },
-    { '<leader>gp', '<cmd>G push<CR>',           desc = '[G]it [P]ush' }
-  }
-}
