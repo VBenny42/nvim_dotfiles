d1eaa0f244bad141389e721db1806a2de254c7b9
Vinesh Benny <vinesh.benny@mail.utoronto.ca>
2023-10-04 17:01:35 -0400

    feat: move to noice, split up notifiers

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,39 @@
+return {
+  'folke/noice.nvim',
+  enabled = true,
+  event = 'VeryLazy',
+  -- add any options here
+  opts = {
+    cmdline = {
+      format = {
+        search_down = { icon = ' ' },
+        search_up = { icon = ' ' }
+      }
+      -- view = 'cmdline'
+    },
+    routes = {
+      {
+        view = 'cmdline',
+        filter = { event = 'msg_showmode' }
+      }
+    },
+    notify = {
+      replace = true,
+      merge = true
+    },
+    lsp = {
+      progress = {
+        enabled = false
+      },
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true
+      }
+    }
+  },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify'
+  }
+}
--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,6 +1,6 @@
 local handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
-  local suffix = (" 󰇘 %d "):format(endLnum - lnum)
+  local suffix = (' 󰇘 %d '):format(endLnum - lnum)
   local sufWidth = vim.fn.strdisplaywidth(suffix)
   local targetWidth = width - sufWidth
   local curWidth = 0
@@ -16,52 +16,52 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
       chunkWidth = vim.fn.strdisplaywidth(chunkText)
       -- str width returned from truncate() may less than 2nd argument, need padding
       if curWidth + chunkWidth < targetWidth then
-        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
       end
       break
     end
     curWidth = curWidth + chunkWidth
   end
-  table.insert(newVirtText, { suffix, "MoreMsg" })
+  table.insert(newVirtText, { suffix, 'MoreMsg' })
   return newVirtText
 end
 
 return {
   {
-    "kevinhwang91/nvim-ufo",
+    'kevinhwang91/nvim-ufo',
     dependencies = {
-      "kevinhwang91/promise-async",
-      "neovim/nvim-lspconfig",
-      "luukvbaal/statuscol.nvim",
+      'kevinhwang91/promise-async',
+      'neovim/nvim-lspconfig',
+      'luukvbaal/statuscol.nvim'
     },
-    --stylua: ignore
+    enabled = false,
     keys = {
-      { "zc" },
-      { "zo" },
-      { "zC" },
-      { "zO" },
-      { "za" },
-      { "zA" },
-      { "zr", function() require("ufo").openFoldsExceptKinds() end, desc = "Open Folds Except Kinds", },
-      { "zR", function() require("ufo").openAllFolds() end,         desc = "Open All Folds", },
-      { "zM", function() require("ufo").closeAllFolds() end,        desc = "Close All Folds", },
-      { "zm", function() require("ufo").closeFoldsWith() end,       desc = "Close Folds With", },
+      { 'zc' },
+      { 'zo' },
+      { 'zC' },
+      { 'zO' },
+      { 'za' },
+      { 'zA' },
+      { 'zr', function() require('ufo').openFoldsExceptKinds() end, desc = 'Open Folds Except Kinds' },
+      { 'zR', function() require('ufo').openAllFolds() end,         desc = 'Open All Folds' },
+      { 'zM', function() require('ufo').closeAllFolds() end,        desc = 'Close All Folds' },
+      { 'zm', function() require('ufo').closeFoldsWith() end,       desc = 'Close Folds With' },
       {
-        "zp",
+        'zp',
         function()
           local winid = require('ufo').peekFoldedLinesUnderCursor()
           if not winid then
             vim.lsp.buf.hover()
           end
         end,
-        desc = "Peek Fold",
-      },
+        desc = 'Peek Fold'
+      }
     },
     opts = {
-      fold_virt_text_handler = handler,
+      fold_virt_text_handler = handler
     },
     config = function(_, opts)
-      require("ufo").setup(opts)
-    end,
-  },
+      require('ufo').setup(opts)
+    end
+  }
 }
